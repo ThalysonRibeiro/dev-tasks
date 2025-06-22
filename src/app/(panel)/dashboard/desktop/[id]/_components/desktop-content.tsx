2ecf231bc6dffe115c4ca0869b506ef9a2d911df
THALYSON RAFAEL <rafinha.head@gmail.com>
2025-06-22 07:25:52 -0300

    refactor header

---
+++ src/app/(panel)/dashboard/desktop/[id]/_components/desktop-content.tsx
@@ -0,0 +1,45 @@
+"use client"
+
+import { useState } from "react";
+import { Header } from "../../_components/header";
+import { CalendarContent } from "./calendar/calendar-content";
+import { GroupContent, GroupWithItems } from "./main-board/group-content";
+import { KanbanContent } from "./kanban/kanbam-content";
+
+interface DesktopContentProps {
+  groupsData: GroupWithItems[];
+  desktopId: string;
+}
+export type TabKey = "main-board" | "kanban" | "calendar";
+export function DesktopContent({ groupsData, desktopId }: DesktopContentProps) {
+  const [activeTab, setActiveTab] = useState<TabKey | string>("main-board");
+
+  const tabsConfig = [
+    {
+      key:
+        "main-board",
+      label: "Quadro principal",
+      component: <GroupContent groupsData={groupsData} desktopId={desktopId} />
+    },
+    {
+      key:
+        "kanban",
+      label:
+        "Kanban",
+      component: <KanbanContent />
+    },
+    {
+      key:
+        "calendar",
+      label:
+        "Calendário",
+      component: <CalendarContent />
+    }
+  ];
+  return (
+    <section className="space-y-6">
+      <Header tabs={tabsConfig} activeTab={activeTab} onTabChange={setActiveTab} />
+      {tabsConfig.find(tab => tab.key === activeTab)?.component}
+    </section>
+  )
+}
