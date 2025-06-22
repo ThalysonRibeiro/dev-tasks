3e434d8687f3121fa674dd6b03899abdbd003af4
THALYSON RAFAEL <rafinha.head@gmail.com>
2025-06-22 07:25:36 -0300

    removed header and DesktopContent

--- src/app/(panel)/dashboard/desktop/[id]/page.tsx
+++ src/app/(panel)/dashboard/desktop/[id]/page.tsx
@@ -1,9 +1,7 @@
 import getSession from "@/lib/getSession";
 import { redirect } from "next/navigation";
-
-import { GroupContent } from "./_components/group-content";
 import { getGroups } from "../../_data-access/get-groups";
-import { Header } from "../../_components/header";
+import { DesktopContent } from "./_components/desktop-content";
 
 
 export default async function DesktopPage({
@@ -19,10 +17,13 @@ export default async function DesktopPage({
   const groupsData = await getGroups({ desktopId: desktopId as string });
 
 
+
   return (
     <main className="container mx-auto px-6 pt-6">
-      <Header />
-      <GroupContent groupsData={groupsData || []} desktopId={desktopId} />
+      <DesktopContent
+        groupsData={groupsData}
+        desktopId={desktopId}
+      />
     </main>
   )
 }
