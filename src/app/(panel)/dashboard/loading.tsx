45896346cf207ec091a5d4de077f65f147fc47bd
THALYSON RAFAEL <rafinha.head@gmail.com>
2025-06-27 19:03:18 -0300

    feat:added loading

---
+++ src/app/(panel)/dashboard/loading.tsx
@@ -0,0 +1,7 @@
+export default function Loading() {
+  return (
+    <div className="absolute top-1/2 left-1/2 z-50">
+      <div className="w-12 h-12 border-4 border-zinc-300 border-b-primary rounded-full animate-spin" />
+    </div>
+  )
+}
