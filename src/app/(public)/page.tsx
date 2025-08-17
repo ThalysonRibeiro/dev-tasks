0b3e23334fdcff6fb689b76c92238d27cdec135f
THALYSON RAFAEL <rafinha.head@gmail.com>
2025-08-17 04:09:23 -0300

    style: add radial in background

--- src/app/(public)/page.tsx
+++ src/app/(public)/page.tsx
@@ -11,9 +11,9 @@ export default async function Home() {
     redirect('/dashboard');
   }
   return (
-    <main className="container mx-auto w-full h-screen flex flex-col gap-6 justify-center items-center px-6">
+    <main className="w-full h-screen flex flex-col gap-6 justify-center items-center px-6 bg-radial from-violet-600/10 via-zinc-950/0 to-zinc-950/0">
       <div className="absolute inset-0 -z-10 bg-[linear-gradient(to_right,#8080800a_1px,transparent_1px),linear-gradient(to_bottom,#8080800a_1px,transparent_1px)] bg-size-[44px_44px]" />
-      <h1 className="text-3xl font-bold uppercase bg-linear-to-r from-violet-500 to-blue-500 bg-clip-text text-transparent inline-flex gap-2 items-center">
+      <h1 className="text-5xl font-bold uppercase bg-linear-to-r from-violet-500 to-blue-500 bg-clip-text text-transparent inline-flex gap-2 items-center">
         <Image
           src={logo_img}
           alt="imagem do logo"
