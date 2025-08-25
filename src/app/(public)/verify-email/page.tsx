1f31e2b6d535ad9c723d7a2eb87b73d1151f6162
THALYSON RAFAEL <rafinha.head@gmail.com>
2025-08-25 17:07:26 -0300

    fix: erro supenmse

--- src/app/(public)/verify-email/page.tsx
+++ src/app/(public)/verify-email/page.tsx
@@ -1,9 +1,12 @@
+import { Suspense } from 'react'
 import { ConfirmEmailConmponent } from "@/app/(public)/verify-email/_components/confirme-email";
 
 export default function VerifyEmailPage() {
   return (
     <div className="container mx-auto px-6 pt-6">
-      <ConfirmEmailConmponent />
+      <Suspense fallback={<div>Carregando...</div>}>
+        <ConfirmEmailConmponent />
+      </Suspense>
     </div>
   )
 }
